7dbff20345dbb6f6f6f1dab7044e97455b920a73
Alecs Geuder <alecs.geuder@bl.uk>
2014-07-16 19:02:29 +0100

    self-contained vm
    * setup script builds a vm that can be run without a vagrant env
      and still has all features available
    * added a welcome page (default host port: 8000) as 'central entry point'

--- .vagrant_settings/setup.sh
+++ .vagrant_settings/setup.sh
@@ -8,6 +8,11 @@
 #     --> it installs c3po and assembles the command-line app
 #     --> it installs and runs a mongodb server
 #     --> it installs and runs the playframework
+#     --> it creates a port-forwarded static welcome page with links
+#         to all the functionalities above
+#     --> it adds all above to the native ubuntu startup, so that an
+#         image of the vm can theoretically be run independently from
+#         the vagrant environment
 
 # [2] in case you're behind a proxy:
 #     --> have a look at http://tmatilai.github.io/vagrant-proxyconf/
@@ -16,6 +21,14 @@
 #         it will be found and copied over to /etc/cntlm.conf; in this
 #         case maven settings.xml has to point to localhost and the port
 #         specified in cntlm.conf, without username and password!
+
+##
+# variables from Vagrantfile
+##
+PLAY_GUEST_PORT=$1
+MONGOD_GUEST_PORT=$2
+MONGOADMIN_GUEST_PORT=$3
+WELCOME_GUEST_PORT=$4
 
 ##
 # variables to be set
@@ -27,6 +40,11 @@ OPEN_JDK=openjdk-7-jdk
 PLAY_VERSION=play-2.0.4
 PLAY_URL=http://download.playframework.org/releases/$PLAY_VERSION.zip
 
+C3PO_LIB=/opt/c3po
+mkdir $C3PO_LIB
+C3PO_SHARE=$C3PO_LIB/welcome
+
+MONGO_VERSION=mongodb-linux-x86_64-2.0.5
 
 ##
 # function definitions
@@ -77,12 +95,42 @@ function setupMaven {
 }
 
 function createCliJar {
-    echo "installing c3po with maven.."
+    echo "[setup.sh] installing c3po with maven.."
     cd /vagrant
     sudo -u vagrant mvn clean install -DskipTests=true
-    echo "assembling jar-with-dependencies"
+    echo "[setup.sh] assembling jar-with-dependencies"
     cd c3po-cmd
     sudo -u vagrant mvn assembly:assembly
+    echo "[setup.sh] copying cmd jar and creating executable file /usr/local/bin/c3po.."
+    cp target/c3po-cmd-*-jar-with-dependencies.jar $C3PO_SHARE/c3po-cmd.jar
+    cp /vagrant/.vagrant_settings/c3po /usr/local/bin/
+    # replace placeholder for C3PO_SHARE in shellscript with real value
+    sed -i.bak s/C3PO_SHARE/$(echo $C3PO_SHARE | sed 's/\//\\\//g')/g /usr/local/bin/c3po
+    chmod +x /usr/local/bin/c3po
+}
+
+function setupC3poShare {
+    echo [setup.sh] setting up http-server for shared files in $C3PO_SHARE
+
+    echo "[setup.sh] ..installing nginx.."
+    apt-get install -y nginx
+
+    echo [setup.sh] ..copying nginx defaults to /etc/nginx/sites-available/default..
+    cp /vagrant/.vagrant_settings/nginx.default /etc/nginx/sites-available/default
+    # replace the placeholder with real c3po_share location
+    sed -i.bak s/C3PO_SHARE/$(echo $C3PO_SHARE | sed 's/\//\\\//g')/g /etc/nginx/sites-available/default
+    # replace the placeholder with real port for the welcome pages
+    sed -i.bak s/WELCOME_GUEST_PORT/$(echo $WELCOME_GUEST_PORT | sed 's/\//\\\//g')/g /etc/nginx/sites-available/default
+
+    echo [setup.sh] ..restarting server..
+    service nginx restart
+}
+
+function setupWelcomePages {
+    echo "setting up the welcome pages with links to c3po-play, the commandline-jar download and mongoadmin pages"
+    cp -r /vagrant/.vagrant_settings/welcome $C3PO_SHARE # this is meant to *create* the directory, so it shouldn't exist yet
+    createCliJar
+    setupC3poShare
 }
 
 function setupMongoDB {
@@ -91,12 +139,13 @@ function setupMongoDB {
 }
 
 function setupMongoDB_2_0_5 {
+    # doesn't work with default ubuntu packages, manual install is one way of doing it
     cd /home/vagrant
     echo "[setup.sh] installing mongodb 2.0.5 (manual install).."
-    wget http://downloads.mongodb.org/linux/mongodb-linux-x86_64-2.0.5.tgz
-    tar xvfz mongodb-linux-x86_64-2.0.5.tgz 
-    rm mongodb-linux-x86_64-2.0.5.tgz 
-    export PATH=$PATH:/home/vagrant/mongodb-linux-x86_64-2.0.5/bin/
+    wget http://downloads.mongodb.org/linux/$MONGO_VERSION.tgz
+    tar xvfz $MONGO_VERSION.tgz
+    rm $MONGO_VERSION.tgz
+    export PATH=$PATH:/home/vagrant/$MONGO_VERSION/bin/
     mkdir -p /data/db
     chown -R vagrant /data/db
     echo "[setup.sh] starting the mongodb server.."
@@ -135,9 +184,33 @@ function runPlay {
     fi
 
     play $PROXY_ARGS clean compile stage
-    echo "[setup.sh] (2) starting the server.."
-    ./target/start &
-    echo "[setup.sh] play started, should be available at localhost:9000/c3po"
+
+    echo [setup.sh] \(2\) copying the compiled files to $C3PO_LIB
+    mkdir $C3PO_LIB/web_target
+    cp -r target/* $C3PO_LIB/web_target
+
+    echo "[setup.sh] (3) starting the server.."
+    $C3PO_LIB/web_target/start &
+    echo [setup.sh] play started in the vm at localhost:$PLAY_GUEST_PORT/c3po
+}
+
+function addAllToNativeStartup {
+    # This makes mongodb, play and the welcome pages run on startup of the created
+    # vm, which is then independent of a vagrant environment.
+    addCustomRcLocal
+
+    # adds nginx, which serves the welcome pages, to startup
+    update-rc.d nginx defaults
+
+}
+
+function addCustomRcLocal {
+    echo "copying a custom version of rc.local over to vm"
+    cp /vagrant/.vagrant_settings/rc.local.custom /etc/rc.local
+    # replace the placeholder with real c3po_lib location
+    sed -i.bak s/C3PO_LIB/$(echo $C3PO_LIB | sed 's/\//\\\//g')/g /etc/rc.local
+    # replace the placeholder with real mongo version
+    sed -i.bak s/MONGO_VERSION/$(echo $MONGO_VERSION | sed 's/\//\\\//g')/g /etc/rc.local
 }
 
 function installGit {
@@ -158,8 +231,9 @@ updateSource
 installJavaDevEnv
 checkProxy
 setupMaven
-createCliJar
 setupMongoDB
 ensureCorrectJavaVersion
 installPlay
 runPlay
+setupWelcomePages
+addAllToNativeStartup
--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,13 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
-AVAILABLE_MEMORY=2048
+AVAILABLE_MEMORY = 2048
+# variable definitions also to be passed on provisioner
+PLAY_HOST_PORT = PLAY_GUEST_PORT = 9000
+MONGOD_HOST_PORT = MONGOD_GUEST_PORT = 27017
+MONGOADMIN_HOST_PORT = MONGOADMIN_GUEST_PORT = 28017
+WELCOME_HOST_PORT = WELCOME_GUEST_PORT = 8000
+
 
 # Vagrantfile API/syntax version. Don't touch unless you know what you're doing!
 VAGRANTFILE_API_VERSION = "2"
@@ -10,12 +16,19 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   config.vm.box = "hashicorp/precise64"
 
-  config.vm.network "forwarded_port", guest: 9000, host: 9000 # playframework
-  config.vm.network "forwarded_port", guest: 27017, host: 27017 # mongodb port
-  config.vm.network "forwarded_port", guest: 28017, host: 28017 # mongodb admin
+  config.vm.network "forwarded_port", guest: PLAY_GUEST_PORT, host: PLAY_HOST_PORT # playframework
+  config.vm.network "forwarded_port", guest: MONGOD_GUEST_PORT, host: MONGOD_HOST_PORT # mongodb port
+  config.vm.network "forwarded_port", guest: MONGOADMIN_GUEST_PORT, host: MONGOADMIN_HOST_PORT # mongodb admin
+  config.vm.network "forwarded_port", guest: WELCOME_GUEST_PORT, host: WELCOME_HOST_PORT # shares vm-internal files
 
-  # Run the provisioning script	
-  config.vm.provision :shell, :path => ".vagrant_settings/setup.sh"
+  # Run the provisioning script
+  SHELL_ARGS = [
+    PLAY_GUEST_PORT,
+    MONGOD_GUEST_PORT,
+    MONGOADMIN_GUEST_PORT,
+    WELCOME_GUEST_PORT,
+  ]
+  config.vm.provision :shell, :path => ".vagrant_settings/setup.sh", :args => "%s %s %s %s" %SHELL_ARGS
 
   config.vm.provider "virtualbox" do |vb|
      vb.customize ["modifyvm", :id, "--memory", AVAILABLE_MEMORY]
