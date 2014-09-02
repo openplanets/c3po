9e5d7291674b5b5ed4b124057809183f8e62c501
Alecs Geuder <alecs.geuder@bl.uk>
2014-09-02 10:25:43 +0100

    basic integration test for tika
    * basic frame's there; needs amending of domain specific rules

--- c3po-cmd/src/test/java/com/petpet/c3po/TikaIntegrationTests.java
+++ c3po-cmd/src/test/java/com/petpet/c3po/TikaIntegrationTests.java
@@ -3,19 +3,32 @@
  */
 package com.petpet.c3po;
 
-import static org.junit.Assert.*;
-
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.Mongo;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.Collection;
+
+import static org.fest.assertions.Assertions.assertThat;
+
 /**
  * @author cfw
  *
  */
 public class TikaIntegrationTests {
+
+    private Mongo mongo;
+    private static final String DATABASE_NAME = "c3po";
+    private static final String TIKA_V1_3_OUTPUT = "/com/petpet/c3po/samples/govdocs/tika1.3-out";
 
 	/**
 	 * @throws java.lang.Exception
@@ -36,6 +49,7 @@ public class TikaIntegrationTests {
 	 */
 	@Before
 	public void setUp() throws Exception {
+        mongo = new Mongo("localhost");
 	}
 
 	/**
@@ -45,9 +59,26 @@ public class TikaIntegrationTests {
 	public void tearDown() throws Exception {
 	}
 
-	@Test
-	public final void test() {
-		fail("Not yet implemented"); // TODO
-	}
+    @Test
+    public final void testTikaVersion1_3() {
+        String testFilesPath = this.getClass().getResource(TIKA_V1_3_OUTPUT).getPath();
+        String[] args = new String[] {"gather",
+                "--collection", "bla",
+                "--inputdir", testFilesPath,
+                "--type", "TIKA"};
+        C3PO.main(args);
+        DB mDB = mongo.getDB(DATABASE_NAME);
+        assertThat(mDB.collectionExists("elements")).isTrue();
+
+        DBCollection mColl = mDB.getCollection("elements");
+        // there's 21 *.tika metadata files
+        assertThat(mColl.count()).isEqualTo(21);
+
+        for (File f: new File(testFilesPath).listFiles()) {
+            DBCursor query = mColl.find(new BasicDBObject("name", f.getName()));
+            // TODO: parse the files, compare the keys/values to what's in the db. this needs domain
+            // specific rules!
+        }
+    }
 
 }
